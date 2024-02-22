0f010728f68ca9dc0f0d15f1d19d07bcd3d74095
Keegan James <68798129+nzkj@users.noreply.github.com>
2024-02-22 22:24:12 +1300

    Add generate AST helper script

---
+++ tools/generate-ast.py
@@ -0,0 +1,14 @@
+from abc import ABC
+
+# Example we want to create
+"""
+Questions:
+Is using the visitor design pattern suited for a Python implementation?
+
+
+`Expr` is the base class for all expression classes.
+    Contains accept(), and all visitors
+"""
+
+class Expr(ABC):
+    pass
