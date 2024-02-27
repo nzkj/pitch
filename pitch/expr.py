96a1e65cebcac563296a092f75c2a38ecae3c3b1
nzkj <nzkj@users.noreply.github.com>
2024-02-27 21:50:01 +1300

    WIP: Broken visitor stuff

--- pitch/expr.py
+++ pitch/expr.py
@@ -1,17 +1,32 @@
 from abc import ABC, abstractmethod
 from Token import Token
 
+
 class Expr(ABC):
     @abstractmethod
-    def accept(self):
+    def accept(self, visitor):
         pass
 
 class Binary(Expr):
-    left: Expr
-    operator: Token
-    right: Expr
+    # def __init__(self, left: Expr, operator: Token, right: Expr):
+    def __init__(self, string: str):
+        # self.left = left
+        # self.operator = operator
+        # self.right = right
+        self.string = string
+    
+    def accept(self, visitor):
+        visitor.visit_binary()
 
-    def __init__(self, left: Expr, operator: Token, right: Expr):
-        self.left = left
-        self.operator = operator
-        self.right = right
+
+class SomeVisitor:
+    def visit_binary(self, binary: Binary):
+        print(f"visited binary {binary.string}")
+
+# define object and visitor
+binary = Binary("some string")
+visitor = SomeVisitor()
+
+# run the SomeVisitor on binary
+binary.accept(visitor)
+
